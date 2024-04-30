e66726ab579216b457a68b7338837264b187b96a
M-UmerAi <168507042+M-UmerAi@users.noreply.github.com>
2024-04-29 23:12:30 -0700

    Add files via upload

---
+++ 12_name.ts
@@ -0,0 +1,10 @@
+
+
+let names :string[] = ['umer akmal','habib ullah','fatir','ali akmal'] ;
+
+const message :string = 'welcome to my AI work: '
+
+for(let i=0; i<names.length; i++){
+    console.log(message + names[i])
+
+}
---
+++ guest_list.ts
@@ -0,0 +1,7 @@
+
+
+let Guest_list :string[] = ['Imran khan','Nawaz Sharif','Altaf hussain'];
+
+for(let i=0; i<Guest_list.length; i++){
+   console.log('Dear Mr. ' + Guest_list[i] + ',\n\nIt is our pleasure to invite you in your party.\n\nThank you!')
+}
