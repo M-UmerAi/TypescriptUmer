3941c1889946668eb356edcbf37e58c6ab029866
M-UmerAi <168507042+M-UmerAi@users.noreply.github.com>
2024-04-29 23:10:45 -0700

    Add files via upload

---
+++ 8,9_favourite_number.ts
@@ -0,0 +1,7 @@
+
+
+const favourite_number :number =8 ;
+
+const message :string = 'Here is your favourite number ' + favourite_number ;
+
+console.log(message);
---
+++ program_1.js
@@ -0,0 +1,5 @@
+"use strict";
+// program written by: umer akmal
+// current date: 11-3-2024
+// this program will write hello world
+console.log('hello world');
