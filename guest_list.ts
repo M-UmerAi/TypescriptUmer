65a8bc0bd30c3b80bc1455c5175122f8602e2dbe
M-UmerAi <168507042+M-UmerAi@users.noreply.github.com>
2024-04-29 23:21:55 -0700

    Add files via upload

--- guest_list.ts
+++ guest_list.ts
@@ -2,6 +2,55 @@
 
 let Guest_list :string[] = ['Imran khan','Nawaz Sharif','Altaf hussain'];
 
-for(let i=0; i<Guest_list.length; i++){
-   console.log('Dear Mr. ' + Guest_list[i] + ',\n\nIt is our pleasure to invite you in your party.\n\nThank you!')
+//for(let i=0; i<Guest_list.length; i++){
+   // console.log('Dear Mr. ' + Guest_list[i] + ',\n\nIt is our pleasure to invite you in your party.\n\nThank you!\n\n')
+// }
+
+
+let absent_Guest :string = 'imran khan' ;
+let new_Guest :string = 'kamran tessori';
+
+Guest_list[0] = new_Guest ;
+//for(let i=0; i<Guest_list.length; i++){
+  // console.log('Dear Mr. ' + Guest_list[i] + ',\n\nIt is our pleasure to invite you in your party.\n\nThank you!\n\n')
+//}
+// console.log(` Mr.${absent_Guest} is not coming to the party.`);
+// console.log('Good News! we find big table so we are inviting 3 more guests.');
+
+Guest_list.unshift('sir zia khan');
+Guest_list.splice(2 , 0, 'maryam Nawaz');
+Guest_list.push('Bilawal Bhutto Zardari');
+
+
+// for(let i=0; i<Guest_list.length; i++){
+   
+//    console.log('Dear Mr. ' + Guest_list[i] + ',\n\nIt is our pleasure to invite you in your party.\n\nThank you!\n\n')
+// }
+
+
+// console.log('\nsorry we can not arrange big table, only two peoples will be invited');
+
+while(Guest_list.length > 2){
+   let remove_Guest =  Guest_list.pop();
+
+// console.log(`sorry mr. ${remove_Guest}, you are not invited for dinner.`);
 }
+// for(let i=0; i<Guest_list.length; i++){
+   
+//    console.log('Dear mr. ' + Guest_list[i] + ',\n\n you are still invited.\n\nThank you!\n\n')
+// }
+ 
+// mene sare guest array se remove kar diye.
+Guest_list.splice(0 , 2);
+console.log(Guest_list);
+
+// Exercise 19
+
+
+console.log(`Total number of guest are: ${Guest_list.length}`);
+
+
+
+
+
+
